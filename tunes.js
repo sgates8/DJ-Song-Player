1b7de4ebb65d67626eaa6b4989a6629321370c8b
Sylvia <sfglamlife@gmail.com>
2020-07-20 11:17:41 -0400

    updated hw

--- tunes.js
+++ tunes.js
@@ -1,4 +1,13 @@
-var playlist =[];
+var playlist=[];
 function playSong(){
+    var songRequest = document.getElementById("request").value;
+    document.getElementById("song").innerHTML = songRequest;
+    playlist.push();
 
+    var radioPlayer = document.getElementById("C:\Users\sfgla\cbc9\week4\Music").src;
+
+}
+
+function removeSong(){
+    playlist.pop();
 }
